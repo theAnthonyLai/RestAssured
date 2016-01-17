2ab6752bf8c25af41d491e71b9eba07946037a16
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-01-16 22:09:14 -0800

    Clean up codes

--- LifeAlert/ViewPatient.swift
+++ LifeAlert/ViewPatient.swift
@@ -26,11 +26,9 @@ class ViewPatient: UIViewController {
         navigationController?.navigationBar.barTintColor = UIColor(red: 0.169, green: 0.729, blue: 0.573, alpha: 1)
         //navigationController?.navigationBar.titleTextAttributes = [UItextAtt]
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
-        
-//        AudioServicesPlayAlertSound(kSystemSoundID_Vibrate)
+
         let myTimer : NSTimer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("fetchAllObjects:"), userInfo: nil, repeats: true)
         
-        //2BBA92
         // Do any additional setup after loading the view.
     }
 
@@ -39,51 +37,6 @@ class ViewPatient: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func updateData(timer : NSTimer) {
-        var query = PFQuery(className:"TestPatient1")
-        query.getObjectInBackgroundWithId("1U0A71XRvn") {
-            (myPatient: PFObject?, error: NSError?) -> Void in
-            if error == nil && myPatient != nil {
-                if (myPatient!["leftShoulder"] as? String == "Check-Pressure") {
-                    self.leftShoulderAlert.hidden = false
-                    self.leftShoulderSafe.hidden = true;
-                } else {
-                    self.leftShoulderAlert.hidden = true
-                    self.leftShoulderSafe.hidden = false
-                }
-                
-                if (myPatient!["rightShoulder"] as? String == "Check-Pressure") {
-                    
-                    self.rightShoulderAlert.hidden = false
-                    self.rightShoulderSafe.hidden = true;
-                } else {
-                    self.rightShoulderAlert.hidden = true
-                    self.rightShoulderSafe.hidden = false
-                }
-                
-                if (myPatient!["leftBack"] as? String == "Check-Pressure") {
-                    self.leftBackAlert.hidden = false
-                    self.leftBackSafe.hidden = true;
-                } else {
-                    self.leftBackAlert.hidden = true
-                    self.leftBackSafe.hidden = false
-                }
-                
-                if (myPatient!["rightBack"] as? String == "Check-Pressure") {
-                    self.rightBackAlert.hidden = false
-                    self.rightBackSafe.hidden = true;
-                } else {
-                    self.rightBackAlert.hidden = true
-                    self.rightBackSafe.hidden = false
-                }
-
-                
-            } else {
-                print(error)
-            }
-        }
-    }
-
     func fetchAllObjects(timer: NSTimer) {
         var query = PFQuery(className: "PatientStatus")
         query.orderByDescending("createdAt")
@@ -159,11 +112,6 @@ class ViewPatient: UIViewController {
 
         }
     }
-    
-    @IBAction func leftShoulderPressed(sender: UIButton) {
-//        let myTimer : NSTimer = NSTimer.scheduledTimerWithTimeInterval(4, target: self, selector: Selector("updateData:"), userInfo: nil, repeats: true)
-    }
-
     
     /*
     // MARK: - Navigation

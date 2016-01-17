245604feddeb1f9d9a11ac6dab30b53b2a591b06
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-01-16 18:41:58 -0800

    Fetch all objects
    Now fetch all objects and check the latest one

--- LifeAlert/ViewPatient.swift
+++ LifeAlert/ViewPatient.swift
@@ -27,7 +27,7 @@ class ViewPatient: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         
         
-        let myTimer : NSTimer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("updateData:"), userInfo: nil, repeats: true)
+        //let myTimer : NSTimer = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: Selector("fetchAllObjects:"), userInfo: nil, repeats: true)
         
         //2BBA92
         // Do any additional setup after loading the view.
@@ -82,6 +82,45 @@ class ViewPatient: UIViewController {
         }
     }
 
+    func fetchAllObjects(timer: NSTimer) {
+        var query = PFQuery(className: "PatientStatus")
+        query.orderByDescending("createdAt")
+        query.getFirstObjectInBackgroundWithBlock {
+            (myPatient: PFObject?, error: NSError?) -> Void in
+            if (myPatient!["leftShoulder"] as? String == "Check-Pressure") {
+                self.leftShoulderAlert.hidden = false
+                self.leftShoulderSafe.hidden = true;
+            } else {
+                self.leftShoulderAlert.hidden = true
+                self.leftShoulderSafe.hidden = false
+            }
+            
+            if (myPatient!["rightShoulder"] as? String == "Check-Pressure") {
+                self.rightShoulderAlert.hidden = false
+                self.rightShoulderSafe.hidden = true;
+            } else {
+                self.rightShoulderAlert.hidden = true
+                self.rightShoulderSafe.hidden = false
+            }
+            
+            if (myPatient!["leftBack"] as? String == "Check-Pressure") {
+                self.leftBackAlert.hidden = false
+                self.leftBackSafe.hidden = true;
+            } else {
+                self.leftBackAlert.hidden = true
+                self.leftBackSafe.hidden = false
+            }
+            
+            if (myPatient!["rightBack"] as? String == "Check-Pressure") {
+                self.rightBackAlert.hidden = false
+                self.rightBackSafe.hidden = true;
+            } else {
+                self.rightBackAlert.hidden = true
+                self.rightBackSafe.hidden = false
+            }
+
+        }
+    }
     
     @IBAction func leftShoulderPressed(sender: UIButton) {
 //        let myTimer : NSTimer = NSTimer.scheduledTimerWithTimeInterval(4, target: self, selector: Selector("updateData:"), userInfo: nil, repeats: true)
